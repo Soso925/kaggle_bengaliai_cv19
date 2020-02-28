934dca49899647b0a2784b045bddc1d67174ffee
Soso925 <tianqiwang.w@gmail.com>
2020-02-28 18:07:59 +0100

    add resize, normalize function

--- preprocessing/preprocessing.py
+++ preprocessing/preprocessing.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
+import cv2
+from tqdm.auto import tqdm
 
-def greyscale_to_rgb(df,h=137, w =236):# prepare X ## Need to replace df_i by train_img_df when working
-    #img_df = df.drop(['image_id'], axis = 1)
+def greyscale_to_rgb(df,h=137, w =236):# prepare X
+
     img_df = df.to_numpy()
     img_df = img_df/255.0
 
@@ -31,3 +33,70 @@ def split_train_test(x, y_gr, y_vd, y_cd, split = 0.8): #split should be greater
   y_cd_training, y_cd_test = y_cd[training_idx], y_cd[test_idx]
   return x_training, x_test,   y_gr_training, y_gr_test ,   y_vd_training, y_vd_test,   y_cd_training, y_cd_test
 
+def normalize_img(df):# prepare X
+    img_df = df.to_numpy()
+    x = img_df/255.0
+    x = x.reshape(-1, h, w, 1)
+    print(x.shape)
+    return x
+
+# resize
+# courtesy of kaggle notebooks
+def resize(df, input_h, input_w, size=64,  need_progress_bar=True):
+    resized = {}
+    resize_size=size
+    if need_progress_bar:
+        for i in tqdm(range(df.shape[0])):
+            image=df.loc[df.index[i]].values.reshape(input_h,input_w)
+            _, thresh = cv2.threshold(image, 30, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            contours, _ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+            idx = 0
+            ls_xmin = []
+            ls_ymin = []
+            ls_xmax = []
+            ls_ymax = []
+            for cnt in contours:
+                idx += 1
+                x,y,w,h = cv2.boundingRect(cnt)
+                ls_xmin.append(x)
+                ls_ymin.append(y)
+                ls_xmax.append(x + w)
+                ls_ymax.append(y + h)
+            xmin = min(ls_xmin)
+            ymin = min(ls_ymin)
+            xmax = max(ls_xmax)
+            ymax = max(ls_ymax)
+
+            roi = image[ymin:ymax,xmin:xmax]
+            resized_roi = cv2.resize(roi, (resize_size, resize_size),interpolation=cv2.INTER_AREA)
+            resized[df.index[i]] = resized_roi.reshape(-1)
+    else:
+        for i in range(df.shape[0]):
+            #image = cv2.resize(df.loc[df.index[i]].values.reshape(137,236),(size,size),None,fx=0.5,fy=0.5,interpolation=cv2.INTER_AREA)
+            image=df.loc[df.index[i]].values.reshape(input_h,input_w)
+            _, thresh = cv2.threshold(image, 30, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+            contours, _ = cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)[-2:]
+
+            idx = 0
+            ls_xmin = []
+            ls_ymin = []
+            ls_xmax = []
+            ls_ymax = []
+            for cnt in contours:
+                idx += 1
+                x,y,w,h = cv2.boundingRect(cnt)
+                ls_xmin.append(x)
+                ls_ymin.append(y)
+                ls_xmax.append(x + w)
+                ls_ymax.append(y + h)
+            xmin = min(ls_xmin)
+            ymin = min(ls_ymin)
+            xmax = max(ls_xmax)
+            ymax = max(ls_ymax)
+
+            roi = image[ymin:ymax,xmin:xmax]
+            resized_roi = cv2.resize(roi, (resize_size, resize_size),interpolation=cv2.INTER_AREA)
+            resized[df.index[i]] = resized_roi.reshape(-1)
+    resized = pd.DataFrame(resized).T
+    return resized
