47bc128003a0d2c9c72615cbe91c901d12e3f0bd
Soso925 <tianqiwang.w@gmail.com>
2020-02-28 18:08:55 +0100

    add model v4: treat 1D data, with size of 64*64 instead of 137*236

--- models/model.py
+++ models/model.py
@@ -1,6 +1,25 @@
 import tensorflow as tf
 from tensorflow.keras.utils import plot_model
 import pickle
+import json
+
+def model_save(model, filename, weight = False):
+    #model is generally huge. Save the model wisely
+    if weight == True :
+        model.save( filename + ".h5")
+        print('saved the model for you in .h5 format' )
+
+    else :
+        model_json = model.to_json()
+        with open(filename + ".json", "w") as json_file:
+            json_file.write(model_json)
+        print('saved the model for you in .json format')
+
+def history_save(fit, filename):
+    #history is always true
+    with open(filename, 'wb') as file_pi:
+        pickle.dump(fit.history, file_pi)
+    print('saved the history for you in pickle format')
 
 def model_toto(summary = False, plot_summary = False) :
     inception = tf.keras.applications.inception_v3.InceptionV3(include_top=False, weights='imagenet', input_tensor=None,
@@ -102,14 +121,66 @@ def model_toto3(summary = False, plot_summary = False) :
         plot_model(model, to_file='model.png')
     return model
 
-def model_save(model, filename, weight = False):
-    #history is always true
-    with open(filename, 'wb') as file_pi:
-        pickle.dump(model.history, file_pi)
-    print('saved the history for you in pickle format')
-    
-    if weight == True :
-        model.save( filename + ".h5")
-        print('saved the model for you in .h5 format' )
 
+def model_v4(input_h, input_w, summary=False, plot_summary=False):
+    inputs = tf.keras.Input(shape=(input_h, input_w, 1))
+    model = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='SAME', activation='relu',
+                                   input_shape=(input_h, input_w, 1))(inputs)
+    model = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(model)
+    model = tf.keras.layers.Conv2D(filters=32, kernel_size=(5, 5), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Dropout(rate=0.2)(model)
 
+    model = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(model)
+    model = tf.keras.layers.Conv2D(filters=64, kernel_size=(5, 5), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.Dropout(rate=0.3)(model)
+
+    model = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=128, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(model)
+    model = tf.keras.layers.Conv2D(filters=128, kernel_size=(5, 5), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.Dropout(rate=0.3)(model)
+
+    model = tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.Conv2D(filters=256, kernel_size=(3, 3), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.MaxPool2D(pool_size=(2, 2))(model)
+    model = tf.keras.layers.Conv2D(filters=256, kernel_size=(5, 5), padding='SAME', activation='relu')(model)
+    model = tf.keras.layers.BatchNormalization(momentum=0.15)(model)
+    model = tf.keras.layers.Dropout(rate=0.2)(model)
+    model = tf.keras.layers.Flatten()(model)
+    model = tf.keras.layers.Dense(1024, activation="relu")(model)
+    model = tf.keras.layers.Dropout(rate=0.3)(model)
+    x = tf.keras.layers.Dense(512, activation="relu")(model)
+
+    # Add a final sigmoid layer for classification
+    head_gr = tf.keras.layers.Dense(168, activation='softmax', name='hgr')(x)
+    head_vd = tf.keras.layers.Dense(11, activation='softmax', name='hvd')(x)
+    head_cd = tf.keras.layers.Dense(7, activation='softmax', name='hcd')(x)
+
+    model = tf.keras.Model(inputs, outputs=[head_gr, head_vd, head_cd])
+
+    model.compile(optimizer='Adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    if summary == True:
+        model.summary()
+    if plot_summary == True:
+        plot_model(model, to_file='model.png')
+
+    return model
